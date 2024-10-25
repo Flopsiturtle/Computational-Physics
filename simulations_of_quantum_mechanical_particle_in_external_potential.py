73398cdd48971ee0b5a20608f29a2d3805fc08b1
Florian Hollants <florianhollants@web.de>
2024-10-25 23:06:31 +0200

    started animation

--- simulations_of_quantum_mechanical_particle_in_external_potential.py
+++ simulations_of_quantum_mechanical_particle_in_external_potential.py
@@ -5,12 +5,15 @@ Created on Wed Oct 23 12:33:12 2024
 @author: Florian Hollants
 """
 import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
 #declaring variables
 H_BAR = .000001
 MASS = .00001
 D = 2
 N = 1000
-A = .00001
+L=1
+A = L/N
 OMEGA=1
 R=1
 T=1
@@ -69,15 +72,27 @@ def hamilton(func):
     """calculating the hamiltonian for double harmonic well"""
     return -1/(2*MU*EPSILON**2)*laplace(func)+potential()
 
+#make sure to save result in regular intervals to use for animation function
 def time_evol(func):
     """time evolution using second-order Fourier transform"""
     for _ in range(M):
         func = func - 1j*hamilton(func)*TAU-TAU**2*hamilton(hamilton(func))/2
     return func
 
-def animate():
+
+fig, ax = plt.subplots()
+line = ax.plot(np.linspace(-L/2, L/2, N), Psi)
+ax.set(xlim=[-L/2,L/2], ylim=[-10,10])
+ax.legend()
+
+def animate(frame):
     """animates time evolution"""
+    y=
     return 
+
+ani = animation.FuncAnimation(fig=fig, func=time_evol, frames=40, interval=30)
+plt.show()
+
 
 #print(Psi)
 print(inner_product(hamilton(Psi),Psi)-inner_product(Psi,hamilton(Psi)))
