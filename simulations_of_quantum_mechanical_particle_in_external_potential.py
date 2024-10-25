393141152d1ea96c48d7a01e71706d0ac500c348
Florian Hollants <florianhollants@web.de>
2024-10-25 09:56:16 +0200

    added Hamilton function and time evolution operator

--- simulations_of_quantum_mechanical_particle_in_external_potential.py
+++ simulations_of_quantum_mechanical_particle_in_external_potential.py
@@ -7,10 +7,26 @@ Created on Wed Oct 23 12:33:12 2024
 import numpy as np
 #declaring variables
 H_BAR = 1
-M = 1
+MASS = 1
 D = 2
 N = 5
 A = 1
+OMEGA=1
+R=1
+T=1
+M=100
+
+
+
+
+
+
+MU=MASS*OMEGA*R**2/H_BAR
+EPSILON = A/R
+TAU=OMEGA*T/M
+
+
+
 
 shape = (N,)  # tuple of D N's to shape ndarrays correctly
 origin = (0,)  # tuple of D zeros to call position in grid later
@@ -20,7 +36,9 @@ for _ in range(D-1):
 
 Psi=np.arange(N**D).reshape(shape).astype('float64')
 
-
+V=np.empty_like(Psi)
+for n, _ in np.ndenumerate(Psi):
+    V[n]=np.dot(np.array(n)-int(N/2),np.array(n)-int(N/2))
 
 
 def laplace(func):
@@ -31,4 +49,13 @@ def laplace(func):
     return lap/A**2
 
 
-print(laplace(Psi))
+def Hamilton(func):
+    return -1/(2*MU*EPSILON**2)*laplace(func)+MU/8*(EPSILON**2*V)
+  
+def time_evol(func):
+    for i in range(M):
+        func = func - Hamilton(func)*TAU-TAU**2*Hamilton(Hamilton(func))/2
+    return func
+
+
+print(time_evol(Psi))
