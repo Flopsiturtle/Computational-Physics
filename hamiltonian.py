f6cdc526a80dd7c2f575dc61c1325348cac2087b
FloTell <floriantelleis@web.de>
2024-10-26 16:09:17 +0200

    working laplace, problems definition hamiltonian

--- hamiltonian.py
+++ hamiltonian.py
@@ -1,25 +1,49 @@
+#Florian Telleis
 import numpy as np
 
-def hamiltonian_lattice(phi,mu,epsilon):
-    D = phi.shape
-    H = np.zeros(D)
+def wave_to_lattice(wave,a):   # transform given wavefunction to lattice units
+    D = len(wave.shape)             #  \\\-> transformation can also be done implicitly in a later function 
+    return a**(D/2)*wave
+
+# phi = wave_to_lattice(wave,a)    # execute for given wavefunction
 
 
 
-test=np.array([[[1,2,6],[2,3,7]],[[2,4,6],[3,4,9]]])
+def laplace_in_lattice(phi):    # input phi has to be in lattice units
+    shape = phi.shape
+    D = len(shape)      # assumption: quadratic arrays; for example 2D: NxN
+    laplace = np.zeros(shape)
+    for i in np.arange(0,D):
+        laplace += np.roll(phi,1,i) - 2*phi + np.roll(phi,-1,i)
+    return laplace
 
-print(test)
-print(np.roll(test,1,0))
+
+def potential_in_lattice(mu,epsilon,n):
+    return mu/8 * (epsilon**2*n**2-1)**2    # ??? whats with n vector?????
+
+
+
+def hamiltonian_in_lattice(wave,a,mu,epsilon,n):    # input phi has to be in lattice units, else phi=a**(D/2)*wave
+    phi = wave_to_lattice(wave,a)
+    H = - 1/(2*mu*epsilon**2)*laplace_in_lattice(phi) + potential_in_lattice(mu,epsilon,n) * phi
+    return H
+
+
+
+
+
+test_3D=np.array([[[2,32],[2,1]],[[2,7],[2,3]]])
+
+print(laplace_in_lattice(test_3D))
+
+
+
+#print(np.arange(0,2))
+
+
+#print(test)
+#print(np.roll(test,1,0))
 
 # axis 2 -> x
 # axis 1 -> y
 # axis 0 -> z
-
-
-
-
-
-
-
-
-#print(np.zeros((2,2,2,2)))
