833425a7b1eab0cf0e8b2dbf4bc47d3acb201bec
FloTell <floriantelleis@web.de>
2024-10-26 13:59:20 +0200

    start

--- hamiltonian.py
+++ hamiltonian.py
@@ -1,9 +1,25 @@
 import numpy as np
 
-1+1
-print(5+5)
-print("5+5")
-print("test")
+def hamiltonian_lattice(phi,mu,epsilon):
+    D = phi.shape
+    H = np.zeros(D)
 
 
-1+2
+
+test=np.array([[[1,2,6],[2,3,7]],[[2,4,6],[3,4,9]]])
+
+print(test)
+print(np.roll(test,1,0))
+
+# axis 2 -> x
+# axis 1 -> y
+# axis 0 -> z
+
+
+
+
+
+
+
+
+#print(np.zeros((2,2,2,2)))
