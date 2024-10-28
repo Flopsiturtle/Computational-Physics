0562dafbcf6779e8b3e60eeea98328121ca40ab8
FloTell <floriantelleis@web.de>
2024-10-28 17:05:12 +0100

    tried n for potential - started checks

--- hamiltonian.py
+++ hamiltonian.py
@@ -1,12 +1,13 @@
 #Florian Telleis
 import numpy as np
+import cmath
 
-def wave_to_lattice(wave,a):   # transform given wavefunction to lattice units
-    D = len(wave.shape)             #  \\\-> transformation can also be done implicitly in a later function 
-    return a**(D/2)*wave
 
-# phi = wave_to_lattice(wave,a)    # execute for given wavefunction
+def wave_to_lattice(wave,a):   # transform given wavefunction to lattice units  \\\-> transformation can also be done implicitly in a later function 
+    D = len(wave.shape)             
+    return a**(D/2)*wave        # assumption: input is a | can also be variant of L=N*a with N=(wave.shape)[0]  \\ [0] is arbitrary because quadratic matrix
 
+# phi = wave_to_lattice(wave,a)
 
 
 def laplace_in_lattice(phi):    # input phi has to be in lattice units
@@ -18,23 +19,58 @@ def laplace_in_lattice(phi):    # input phi has to be in lattice units
     return laplace
 
 
-def potential_in_lattice(mu,epsilon,n):
-    return mu/8 * (epsilon**2*n**2-1)**2    # ??? whats with n vector?????
+def potential_in_lattice(mu,epsilon,phi):
+    D = len(phi.shape)
+    n = np.zeros(D)
+#    for i in np.arange(D):                     # ??? whats with n vector?????
+#        ....
+    V = mu/8 * (epsilon**2*n@n-1)**2       # you could create array of all the n@n and than np.multiply with phi
+    return V            
 
 
 
-def hamiltonian_in_lattice(wave,a,mu,epsilon,n):    # input phi has to be in lattice units, else phi=a**(D/2)*wave
+def hamiltonian_in_lattice(wave,a,mu,epsilon,n):    # phi has to be in lattice units, else phi=a**(D/2)*wave
     phi = wave_to_lattice(wave,a)
     H = - 1/(2*mu*epsilon**2)*laplace_in_lattice(phi) + potential_in_lattice(mu,epsilon,n) * phi
     return H
 
 
 
+# check for characteristics: - linear, - hermitian, - 
+
+def check_hermitian(hamilton):      # geht nur für 2D arrays durch np.matrix!!!
+    ham_matrix = np.matrix(hamilton)
+    ham_adj = ham_matrix.getH()
+    ham_adj_matrix = np.matrix(ham_adj)
+    if (ham_matrix == ham_adj_matrix).all():
+        a = "hermitian"
+    else:
+        a = "non-hermitian"
+    return a
+
+def check_linear(hamilton):
+    # hamilton(lambda*phi) == lambda*hamilton(phi)      # -> welche Zahl als Beispiel?
+    # hamilton(phi+phi´) == hamilton(phi) + hamilton(phi´)      # wie phi´ aus phi erzeugen?
 
 
-test_3D=np.array([[[2,32],[2,1]],[[2,7],[2,3]]])
+
+# ----- tests -----
+
+
+test_2D = np.array([[[1,1],[1,1]]])
+test_2D2 = np.array([[[2,5+1j],[3,9+4j]]])
+test_3D = np.array([[[2,32],[2,1]],[[2,7],[2,3]]])
+
 
 print(laplace_in_lattice(test_3D))
+
+#print(check_hermitian(test_3D)) # geht nicht durch 3D
+
+print(check_hermitian(test_2D))
+print(check_hermitian(test_2D2))
+
+
+
 
 
 
---
+++ testing stuff.py
@@ -0,0 +1,29 @@
+import numpy as np
+
+aa = np.array([[0,1,2],[5,6,7],[11,12,13]])
+print(aa)
+shape_a = aa.shape
+
+print((aa.shape)[0])
+
+print(np.zeros(len(shape_a)))
+
+print(aa[1,1])
+
+
+bb = np.array([[0,1,2],[5,6,7],[11,12,13]])
+
+print(np.multiply(aa,bb))
+
+print(np.arange(2))
+
+D = len(aa.shape)
+n = np.zeros(D)
+n_vector = np.zeros(aa.shape)
+
+#for i in np.arange(D):
+#    for j in np.arange((aa.shape)[i]):
+#        n_vector[j] = 
+#print(n_vector)
+
+print(np.mgrid[:5,:5].transpose(1,2,0))
