99aa8487526858db6a656ea2223d0f712b20e26f
Florian Hollants <florianhollants@web.de>
2024-11-07 16:47:13 +0100

    working animation. Next steps are finishing tests, adding strang splitting and using gaussians

--- simulations_of_quantum_mechanical_particle_in_external_potential.py
+++ simulations_of_quantum_mechanical_particle_in_external_potential.py
@@ -7,49 +7,66 @@ Created on Wed Oct 23 12:33:12 2024
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
-import scipy.constants as const
-#declaring variables
-H_BAR = .0001 #const.hbar*10**5
-MASS = .0001 #const.electron_mass*10**5
-D = 1
-N = 100
-L = 1#N*const.physical_constants['atomic unit of length'][0]*10**5
-A = L/N#const.physical_constants['atomic unit of length'][0]*10**5
-OMEGA=1#/const.physical_constants['atomic unit of time'][0]*10**5
-R=L/20
-T=1#3*const.physical_constants['atomic unit of time'][0]*10**5
-M=1000
+#import scipy.constants as const
 
 
 
-MU=MASS*OMEGA*R**2/H_BAR
-EPSILON = A/R
-TAU=OMEGA*T/M
+def initialize(gauss = False, random = False):
+    """declaring variables and initializing Psi"""
+    global H_BAR, MASS, D, N, L, A
+    H_BAR = .001
+    MASS = 1
+    D = 1
+    N = 3000
+    L = 2
+    A = L/N
+    global OMEGA, R, T, M
+    OMEGA=1
+    R=L/4
+    T=1
+    M=1000
+    global MU, EPSILON, TAU, sig, mu
+    MU=MASS*OMEGA*R**2/H_BAR
+    EPSILON = A/R
+    TAU=OMEGA*T/M #
+    sig = R/10
+    mu = int(N/2)
+    global shape, origin, Psi
+    shape = (N,)  # tuple of D N's to shape ndarrays correctly
+    origin = (0,)  # tuple of D zeros to call position in grid later
+    for _ in range(D-1):
+        shape += (N,)
+        origin += (0,)
+    
+    Psi =np.arange(N**D).reshape(shape).astype('complex')*0+1
+    
+    if random:
+        Psi = np.random.uniform(-1,1,shape)+1j*np.random.uniform(-1,1,shape)
+        return()
+    """
+    if gauss:
+        for n, _ in np.ndenumerate(Psi):
+            x = (np.array(n) - mu) * A
+            Psi[n] = 1.0 / (np.sqrt(2.0 * np.pi) * sig) * np.exp(-np.dot(x, x) / sig / 2) + 0j
+        return()"""
+    return()
 
 
-
-
-shape = (N,)  # tuple of D N's to shape ndarrays correctly
-origin = (0,)  # tuple of D zeros to call position in grid later
-for _ in range(D-1):
-    shape += (N,)
-    origin += (0,)
-
-Psi=np.arange(N**D).reshape(shape).astype('complex')/N**D
-
-#Psi=np.random.uniform(-1,1,shape)+1j*np.random.uniform(-1,1,shape)
-#Phi=np.random.uniform(-1,1,shape)+1j*np.random.uniform(-1,1,shape)
+initialize(gauss = False, random = False)
 #shifting the origin to the center of the grid
-
-
 V=np.empty_like(Psi)
 for n, _ in np.ndenumerate(Psi):
     V[n]=np.dot(np.array(n)-int(N/2),np.array(n)-int(N/2))
 
 
+
 def potential():
     """defines the potential"""
-    return MU/8*(EPSILON**2*V-1)**2
+    V=np.empty_like(Psi)*0
+    for n, _ in np.ndenumerate(Psi):
+        index_arr = np.array(n)
+        V[n]=MU/8*(EPSILON**2*np.dot(index_arr-int(N/2),index_arr-int(N/2))-1)**2
+    return V
 
 
 def inner_product(func1, func2):
@@ -57,7 +74,9 @@ def inner_product(func1, func2):
     return np.sum(np.multiply(np.conjugate(func1),func2))
 
 
-Psi *= 1/np.sqrt(inner_product(Psi,Psi))
+def normalize(func):
+    """normalizes input function"""
+    return func*1/np.sqrt(inner_product(func,func))
 
 
 def laplace(func):
@@ -71,33 +90,117 @@ def laplace(func):
 
 def hamilton(func):
     """calculating the hamiltonian for double harmonic well"""
-    return -1/(2*MU*EPSILON**2)*laplace(func)+potential()
+    return -1/(2*MU*EPSILON**2)*laplace(func)+potential()*func
 
-#make sure to save result in regular intervals to use for animation function
+
 def time_evol(func, n):
     """time evolution using second-order Fourier transform"""
     for _ in range(n):
         func = func - 1j*hamilton(func)*TAU-TAU**2*hamilton(hamilton(func))/2
     return func
 
+def check_linearity(func):
+    res = 0
+    n = 100
+    for _ in range(n):
+        Psi=np.random.uniform(-1,1,shape)+1j*np.random.uniform(-1,1,shape)
+        Phi=np.random.uniform(-1,1,shape)+1j*np.random.uniform(-1,1,shape)
+        res += np.abs(np.max(np.abs(func(2*Psi+Phi)-2*func(Psi)-func(Phi))))
+    if res < 1/10**9:
+        print(func.__name__ + " is linear")
+        return res/n
+    else:
+        print(func.__name__ + " is not linear")
+        print(res)
+        return res/n
+
+
+def check_hermitian(func):
+    res = 0
+    n = 100
+    for _ in range(n):
+        Psi=np.random.uniform(-1,1,shape)+1j*np.random.uniform(-1,1,shape)
+        res += np.abs(inner_product(func(Psi),Psi)-inner_product(Psi,func(Psi)))
+    if res < 1/10**9:
+        print(func.__name__ + " is hermitian")
+        return res/n
+    else:
+        print(func.__name__ + " is not hermitian")
+        return res/n
+
+
+def check_positivity(func):
+    count = 0
+    n = 100
+    for _ in range(n):
+        Psi=np.random.uniform(-1,1,shape)+1j*np.random.uniform(-1,1,shape)
+        if inner_product(Psi,func(Psi)).real < 0:
+            count += 1
+    print("The Energy has been negative " + str(count) + " out of " + str(n) + " times")
+    return count
+
+
+def check_eigen():
+    k = [np.random.uniform(-1,1)+1j*np.random.uniform(-1,1) for _ in range(D)]
+    for n, _ in np.ndenumerate(Psi):
+        Psi[n]=np.exp(2j*np.pi*np.dot(np.array(n)-int(N/2),k)/N)
+    E = D/(MU*EPSILON**2)
+    for _ in range(D):
+        E += -1/(2*MU*EPSILON**2)*(Psi[int(N/2)+1]+Psi[int(N/2)-1])
+    print(E)
+    return()
+#print(np.eye(1, D, 0))
+
+#check_eigen()
+
+
+Psi = normalize(Psi)
+
+
 
 fig, ax = plt.subplots()
-line = ax.plot(np.linspace(-L/2, L/2, N), Psi.imag)[0]
-ax.set(xlim=[-L/2,L/2], ylim=[-10*10**5*10**5,10*10**5*10**5])
 
-def animate(frame):
-    """animates time evolution"""
-    line.set_ydata(time_evol(Psi, n=frame).imag)
-    return line
+line2 = ax.plot(np.linspace(-L/(2*R), L/(2*R), N), np.abs(Psi)**2/np.sqrt(EPSILON))[0]
+ax.set(xlim=[-L/(2*R), L/(2*R)], ylim=[-1, 1])
 
-#ani = animation.FuncAnimation(fig=fig, func=animate, frames=50, interval=1000)
-#plt.show()
+
+
+t = [0]
+Norm = [inner_product(Psi, Psi).real]
+E = [inner_product(Psi, hamilton(Psi)).real]
+
+
+
+running = Psi
+
+def update(frame):
+    # for each frame, update the data stored on each artist.
+    # update the line plot:
+    global running
+    global t
+    global Norm
+    global E
+    running = time_evol(running, 10)
+    line2.set_ydata(np.abs(running)**2/np.sqrt(EPSILON))
+    t.append(len(t)*10)
+    Norm.append(inner_product(running, running))
+    E.append(inner_product(running, hamilton(running)))
+    return (line2)
+
+
+ani = animation.FuncAnimation(fig=fig, func=update, frames=100, interval=30)
+plt.show()
+
+plt.plot(t, Norm, E)
+#check_linearity(hamilton)
+#check_hermitian(hamilton)
+#check_positivity(hamilton)
 
 
 #print(Psi)
-#(inner_product(hamilton(Psi),Psi)-inner_product(Psi,hamilton(Psi)))
-#print(np.average(hamilton(2*Psi+Phi)-2*hamilton(Psi)-hamilton(Phi)))
+#print(inner_product(hamilton(Psi),Psi)-inner_product(Psi,hamilton(Psi)))
+#print(max(np.abs(hamilton(2*Psi+Phi)-2*hamilton(Psi)-hamilton(Phi))))
 #print(laplace(Psi))
 #print(hamilton(Psi))
-print(time_evol(Psi, 20))
-print(inner_product(time_evol(Psi, 20), time_evol(Psi, 20)))
+#print(time_evol(Psi, 20))
+#print(inner_product(time_evol(Psi, 20), time_evol(Psi, 20)))
