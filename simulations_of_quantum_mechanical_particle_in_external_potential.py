d22b2e5bf4758e2f9b674bf0424f2fb78364a130
Flopsiturtle <80710942+Flopsiturtle@users.noreply.github.com>
2024-11-10 22:10:46 +0100

    Update simulations_of_quantum_mechanical_particle_in_external_potential.py

--- simulations_of_quantum_mechanical_particle_in_external_potential.py
+++ simulations_of_quantum_mechanical_particle_in_external_potential.py
@@ -3,8 +3,12 @@
 Created on Wed Oct 23 12:33:12 2024
 
 @author: Florian Hollants
+
+Goal: Animate Wavefunction that starts as gaussian centered at one minimum of the potential
 """
 import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
 #declaring variables
 H_BAR = 1
 M = 1
@@ -20,15 +24,32 @@ for _ in range(D-1):
 
 Psi=np.arange(N**D).reshape(shape).astype('float64')
 
+# Create the potential
+V=np.empty_like(Psi)
+for n, _ in np.ndenumerate(Psi):
+    V[n]=np.dot(np.array(n)-int(N/2),np.array(n)-int(N/2))
 
+def potential():
+    """defines the potential"""
+    V=np.empty_like(Psi)*0
+    for n, _ in np.ndenumerate(Psi):
+        index_arr = np.array(n)
+        V[n]=MU/8*(EPSILON**2*np.dot(index_arr-int(N/2),index_arr-int(N/2))-1)**2
+    return V
 
 
 def laplace(func):
+    """calculating the laplacian of ndarray"""
     lap = -2*D*func
     for j in range(D):
         lap += (np.roll(func, -1, axis=j)
                 +np.roll(func, 1, axis=j))
-    return lap/A**2
+    return lap
 
 
-print(laplace(Psi))
+def hamilton(func):
+    """calculating the hamiltonian for double harmonic well"""
+    return -1/(2*MU*EPSILON**2)*laplace(func)+potential()*func
+
+
+
