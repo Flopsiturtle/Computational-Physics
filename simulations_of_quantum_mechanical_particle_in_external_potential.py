a22347ddfbdd318b2c2c198d5db730c0c6a319d3
FloTell <floriantelleis@web.de>
2024-11-13 16:25:28 +0100

    Add files via upload

--- simulations_of_quantum_mechanical_particle_in_external_potential.py
+++ simulations_of_quantum_mechanical_particle_in_external_potential.py
@@ -1,26 +1,23 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Wed Oct 23 12:33:12 2024
+# Authors: Florian Telleis, Florian Hollants, Mickey Wilke
 
-@author: Florian Hollants
-
-Goal: Animate Wavefunction that starts as gaussian centered at one minimum of the potential
-"""
 import numpy as np
 from scipy.fftpack import fft, ifft
 from scipy import stats 
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
-#declaring variables
+
+
 N = 200 # number of points in each direction of D-dimensional lattice  # if wavefunction is given: N = (wave.shape)[0]    # \\ [0] is arbitrary because quadratic matrix
 A = 0.3  # spacing between lattice points   # assumption: A is input | can also be variant of L=N*a  
-"""potential/hamiltonian"""
+
 R = 18  # length from zero-point to potential-valleys 
 M = 0.1   # mass of point particle
 W = 5   # frequency
 H_BAR = 1#!!! actually: 6.62607015*10**(-34)    # J*s
+
 mu = (M*W*R**2)/H_BAR
 epsilon = A/R
+
 D=1
 M = 10000  # large value
 T = 10      # time
@@ -31,11 +28,6 @@ FPS = int(FRAMES/T)     # number of frames per second if given time T is real ti
 #FPS = 23
 
 
-shape = (N,)  # tuple of D N's to shape ndarrays correctly
-origin = (0,)  # tuple of D zeros to call position in grid later
-for _ in range(D-1):
-    shape += (N,)
-    origin += (0,)
 
 
 def gaussian_1D(mean,sigma): 
@@ -60,15 +52,17 @@ def laplace(func):
         lap += (np.roll(func, -1, axis=j)
                 +np.roll(func, 1, axis=j))
     return lap
+
+
 def kinetic_hamilton(func):
     """calculating the free_hamiltonian"""
     return -1/(2*mu*epsilon**2)*laplace(func)
 
 
-
 def hamilton(func):
     """calculating the hamiltonian for double harmonic well"""
     return -1/(2*mu*epsilon**2)*laplace(func)+np.multiply(V,func)
+
 
 def test_linearity(Hamiltonian,psi_in,iterations):
     shape = np.shape(psi_in)
@@ -93,12 +87,14 @@ def test_hermiticity(Hamiltonian, psi_in, iterations):
         RHS = np.sum(np.multiply(np.conjugate(Hamiltonian(psi1)), psi2))
         print(np.abs(LHS - RHS))
 
+
 def test_positivity(Hamiltonian, psi_in, iterations):
     shape = np.shape(psi_in)
     for i in range(iterations):
         psi1 = np.random.rand(*shape) + 1j * np.random.rand(*shape)
         print("Potential:"  ,np.sign(np.sum(np.multiply(np.conjugate(psi1), potential(psi1))).real))
         print("Hamiltonian:" , np.sign(np.sum(np.multiply(np.conjugate(psi1), Hamiltonian(psi1))).real))
+
 
 def test_eigenvectors(Kinetic_Hamiltonian, psi_in, iterations):
     shape = np.shape(psi_in)
@@ -118,8 +114,9 @@ def test_eigenvectors(Kinetic_Hamiltonian, psi_in, iterations):
 
 
 
+
 def so_integrator(func,M):
-    """solves the time dependent schrödinger equation for a given wavefunction "phi0" with the second-order integrator"""
+    """solves the time dependent schrödinger equation for a given wavefunction with the second-order integrator"""
     start = func
     for m in np.arange(0,M):
         iteration = start - 1j*tau*hamilton(start) - 1/2*tau**2*hamilton(hamilton(start))
@@ -128,17 +125,6 @@ def so_integrator(func,M):
 
 
 
-
-def images(func, integ):
-    """ creates a list of the calculated wavefunctions for all timesteps of the second-order time evolution """
-    start = func
-    ims = []
-    ims.append(start)
-    for m in np.arange(0,M):
-        iteration = integ(start,1)
-        start = iteration
-        ims.append(iteration)
-    return ims
 
 def Exponential_potential(psi_in, time_step):
     shape = np.shape(psi_in)
@@ -149,7 +135,6 @@ def Exponential_potential(psi_in, time_step):
     psi_out = np.exp(-1j*time_step/2 * potential_term)
     psi_out = np.multiply(psi_out, psi_in)
     return psi_out
-
 
 
 def Exponential_kinetic(psi_in, time_step):
@@ -163,7 +148,6 @@ def Exponential_kinetic(psi_in, time_step):
     return psi_out
 
 
-
 def Strang_Splitting(psi_in,M):
     for i in range(M):
         psi_out = Exponential_potential(psi_in, tau)
@@ -175,6 +159,7 @@ def Strang_Splitting(psi_in,M):
     return psi_out
 
 
+
 def test_unitarity(psi_in, iterations):
     shape = np.shape(psi_in)
     phi = np.random.rand(*shape) + 1j * np.random.rand(*shape)
@@ -184,6 +169,7 @@ def test_unitarity(psi_in, iterations):
         norm = np.sqrt(np.dot(np.conjugate(wave), wave))
         print(np.abs(1-norm))
         phi = wave
+
 
 def test_Strang_linearity(psi_in, iterations):
     shape = np.shape(psi_in)
@@ -197,6 +183,7 @@ def test_Strang_linearity(psi_in, iterations):
         error = np.sum(np.abs(LHS - RHS))
         print(error)
        
+    
 def test_energy_conserv(psi_in, iterations):
     shape = np.shape(psi_in)
     phi = np.random.rand(*shape) + 1j * np.random.rand(*shape)
@@ -213,83 +200,73 @@ def test_energy_conserv(psi_in, iterations):
 
 
 
+def images(func, integ):
+    """ creates a list of the calculated wavefunctions for all timesteps """
+    start = func
+    ims = []
+    ims.append(start)
+    for m in np.arange(0,M):
+        iteration = integ(start,1)
+        start = iteration
+        ims.append(iteration)
+    return ims
 
 
 
-#fig = plt.figure()
-#axis = plt.axes(xlim=(0,N-1),ylim =(0, 0.002))  # for constant axis
 
-#fig, axis = plt.subplots()     # for variable axis
 
-fig, (ax1, ax2) = plt.subplots(1,2)
+
+fig, (ax1, ax2,ax3) = plt.subplots(1,3, figsize=(16, 9))
+
 line1, = ax1.plot([], [])  
-line2, = ax2.plot([], [])  
+line2, = ax2.plot([], [],'green')
+line3, = ax3.plot([], [],'orange')  
+
 ax1.set_xlim(0,N-1)
 ax1.set_ylim(0,0.4)
 ax2.set_xlim(0,N-1)
 ax2.set_ylim(0,0.4)
+ax3.set_xlim(0,N-1)
+ax3.set_ylim(0,0.002)
 
-def init1():  
-    line1.set_data([], []) 
-    return line1, 
+fig.suptitle('mu={0}, epsilon={1}, N={2}, M={3}, T={4}, tau={5}'.format(mu,epsilon,N,M,T,tau), fontsize=15)
+ax1.set_title("Second-order integrator")
+ax2.set_title("Strang-splitting integrator")
+ax3.set_title("Difference between both integrators")
 
-def init2():  
-    line2.set_data([], []) 
-    return line2, 
-   
-   
-def animate_so_integrator(i): 
-    """function that gets called for animation"""
-    """takes a number of arrays from calculated "images" corresponding to FRAMES and sets a line data"""
-    global FRAMES,images_so,M
-    i2 = int(i*M/(FRAMES-1)) # dont use all the frames from the time evolution
-    y = abs(images_so[i2])**2
+
+def animate(y,line):
     x = np.arange(0,len(y)) 
-    #axis.set_ylim(0, max(y)*1.1) # for variable axis
-    line1.set_data(x, y) 
-    return line1, 
+    line.set_data(x,y)
 
-def animate_strang_integrator(i): 
-    """function that gets called for animation"""
-    """takes a number of arrays from calculated "images" corresponding to FRAMES and sets a line data"""
-    global FRAMES,images_strang,M
-    i2 = int(i*M/(FRAMES-1)) # dont use all the frames from the time evolution
-    y = abs(images_strang[i2])**2
-    x = np.arange(0,len(y)) 
-    #axis.set_ylim(0, max(y)*1.1) # for variable axis
-    line2.set_data(x, y) 
-    return line2, 
+def animate_all(i):
+    i2 = int(i*M/(FRAMES-1))    # dont use all the frames from the time evolution
+    animate(abs(images_so[i2])**2 , line1)      # second-order
+    animate(abs(images_strang[i2])**2 , line2)      # strang-splitting
+    animate(abs(abs(images_strang[i2])**2-abs(images_so[i2])**2) , line3)   # difference between both
+    return line1, line2, line3,
 
 
-Psi=gaussian_1D(25,10)*10
+
+
+""" --- run the code --- """
+
+Psi=gaussian_1D(25,10) * 10     ###### anstatt * 10 lieber normieren!!!
 V = potential(Psi)
+
+
 iterations = 10
-
-
 ##### all the tests - with naming of them in the print()
 
 
 
-images_so = images(Psi, so_integrator)    # creates the list of arrays for our test function
+
+images_so = images(Psi, so_integrator) 
 images_strang = images(Psi, Strang_Splitting)
 
+anim = animation.FuncAnimation(fig, animate_all, frames = FRAMES, interval = 1000/FPS, blit = True) 
 
-
-anim = animation.FuncAnimation(fig, animate_so_integrator, init_func = init1, frames = FRAMES, interval = 1000/FPS, blit = False) 
-#plt.show()
-anim2 = animation.FuncAnimation(fig, animate_strang_integrator, init_func = init2, frames = FRAMES, interval = 1000/FPS, blit = False) 
-
-
-
-""" difference in last M for our tau"""
-""" question is: do we run the code three times for different tau???"""
-
-fig = plt.figure()
-axis = plt.axes(xlim=(0,N-1),ylim =(0, 0.002))  # for constant axis
-axis.plot(np.arange(0,200),np.abs(abs(images_so[M])**2-abs(images_strang[M])**2))
-axis.set_ylim(0, max(np.abs(abs(images_so[M])**2-abs(images_strang[M])**2))*1.1) 
-#plt.plot(np.arange(0,200),np.abs(abs(images_so[M])**2-abs(images_strang[M])**2))
-
+#anim.save('animation_project.gif', writer = 'pillow', fps = FPS) 
 
 
 plt.show()
@@ -307,21 +284,51 @@ plt.show()
 
 
 
+"""
+def init1():  
+    line1.set_data([], []) 
+    return line1, 
 
+def init2():  
+    line2.set_data([], []) 
+    return line2, 
+   
+def init3():  
+    line3.set_data([], []) 
+    return line3,    
+   
+def animate_so_integrator(i): 
+    function that gets called for animation"""
+"""takes a number of arrays from calculated "images" corresponding to FRAMES and sets a line data
+    global FRAMES,images_so,M
+    i2 = int(i*M/(FRAMES-1)) # dont use all the frames from the time evolution
+    y = abs(images_so[i2])**2
+    x = np.arange(0,len(y)) 
+    #ax1.set_ylim(0, max(y)*1.1) # for variable axis
+    line1.set_data(x, y) 
+    return line1, 
 
+def animate_strang_integrator(i): 
+    function that gets called for animation"""
+"""takes a number of arrays from calculated "images" corresponding to FRAMES and sets a line data
+    global FRAMES,images_strang,M
+    i2 = int(i*M/(FRAMES-1)) # dont use all the frames from the time evolution
+    y = abs(images_strang[i2])**2
+    x = np.arange(0,len(y)) 
+    #ax2.set_ylim(0, max(y)*1.1) # for variable axis
+    line2.set_data(x, y) 
+    return line2, 
 
-
-
-
-
-
-
-
-
-
-
-
-
+def animate_difference(i): 
+    function that gets called for animation"""
+"""takes a number of arrays from calculated "images" corresponding to FRAMES and sets a line data
+    global FRAMES,images_so,images_strang,M
+    i2 = int(i*M/(FRAMES-1)) # dont use all the frames from the time evolution
+    y = abs( abs(images_strang[i2])**2 - abs(images_so[i2])**2 )
+    x = np.arange(0,len(y)) 
+    #ax3.set_ylim(0, max(y)*1.1) # for variable axis
+    line3.set_data(x, y) 
+    return line3,"""
 
 
 
