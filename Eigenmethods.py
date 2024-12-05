e6e330b3969a2c2f02bbf76449fe0b9ec531ae72
FloTell <floriantelleis@web.de>
2024-12-05 16:16:05 +0100

    changed to np.vdot()

--- Eigenmethods.py
+++ Eigenmethods.py
@@ -11,8 +11,7 @@ import hamiltonian
 # A x = b
 # H x = v -> x = H^-1 v
 # so conjugate gradient gives x as result which is our wanted quantity
-
-# implement hamiltonian in function itself
+# now: implement hamiltonian in function itself
 
 
 def Hinv(v,tolerance,maxiters):
@@ -23,38 +22,41 @@ def Hinv(v,tolerance,maxiters):
         return x0
     p0 = r0
     for i in np.arange(1,maxiters+1):
-        alpha0 = (variables.inner_product(np.transpose(r0),r0)) / (variables.inner_product(np.transpose(p0),hamiltonian.hamilton(p0)))    # do i still have to transpose like wikipedia?
+        alpha0 = (np.vdot(r0,r0)) / (np.vdot(p0,hamiltonian.hamilton(p0)))    
         x = x0 + alpha0*p0
         r = r0 - alpha0*hamiltonian.hamilton(p0)
         if np.max(r) <= tolerance: 
             return x,i
-        beta = (variables.inner_product(np.transpose(r),r)) / (variables.inner_product(np.transpose(r0),r0))
+        beta = (np.vdot(r,r)) / (np.vdot(r0,r0))
         p0 = r + beta*p0
         x0 = x
         r0 = r
-        #if i == maxiters:   # or enough without if statement, because output is "None"???
-        #    return "Maximum iterations of " + str(maxiters) + " reached."
+
+
+####### for using the result in Arnoldi method: use Hinv()[0] because I am also returning the iteration as second argument
 
 
 
-""" --- run the code --- """
 
-#### 1D
+
+
+
+""" --- test the code --- """
+
+# tests for arbitrary v
+#### 1D test
 v = np.ones(N)     
 error = 0.00001
 max_integers = 200
-
-print(Hinv(v,error,max_integers))
-
-#### 1D test for complex
-n, Psi=variables.gaussian_1D(-int(N/4),int(N/16))
-Psi = variables.normalize(Psi)
-v = Psi
-
 print(Hinv(v,error,max_integers))
 
 
+#### 1D test for complex with our gaussian
+n, v=variables.gaussian_1D(-int(N/4),int(N/16))
+v = variables.normalize(v)
+print(Hinv(v,error,max_integers))
 
-# 2D    ######## doesnt work in 2D i think?
+
+# 2D test    ######## method doesnt work in 2D i think? but also does not need to work in 2D
 #v = np.ones((N,N))
-#print(Hinv(v,0.001,100))
+#print(Hinv(v,0.001,10))
