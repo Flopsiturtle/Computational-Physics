07fadf3dfdd87830d9bc99a889be221b1a0220a7
FloTell <floriantelleis@web.de>
2024-12-09 13:23:05 +0100

    Update test_project2.py

--- test_project2.py
+++ test_project2.py
@@ -1,11 +1,13 @@
 
-#### test if inverse is correct
+
+
+#Flo T ### test if inverse is correct
 
 ###### Tests:
-### test if really eigenvalues/vectors
-### test if relly orthonormal
-#Mickey ## test if really lowest eigenvalue (?)
-### test our results with linalgs programs
+#Flo T ## test if really eigenvalues/vectors
+#Flo H ## test if really orthonormal
+#Mickey ## test if really lowest eigenvalue
+#xxx ## test our results with linalgs programs (?)
 
-### test with matrices we know eigenvalues/vectors -->> hamiltonians from different systems
+#Mickey/Flo H ## test with matrices we know eigenvalues/vectors -->> hamiltonians from different systems
 ### .....
