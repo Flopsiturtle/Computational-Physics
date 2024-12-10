e3530664d8a33318e6d3ffdbc66a60cdac53d807
FloTell <floriantelleis@web.de>
2024-12-10 20:14:09 +0100

    tried my tests

--- test_eigenmethods.py
+++ test_eigenmethods.py
@@ -6,34 +6,40 @@ import hamiltonian
 
 ''' --- #Flo T ### test if inverse is correct --- '''
 def test_Hinv_inverse(v,error_Hinv,maxiters_Hinv):
-    err = np.abs(np.vdot(hamiltonian.hamilton(v),eigenmethods.Hinv(v,error_Hinv,maxiters_Hinv)))   # ist das wirklich richtig für inverse??
+    err = np.abs(hamiltonian.hamilton(v)*eigenmethods.Hinv(v,error_Hinv,maxiters_Hinv))   # ist das wirklich richtig für inverse??
     return err
 
-v=np.ones(50)
-#print(hamiltonian.hamilton(v))
-#print(eigenmethods.Hinv(v,0.0001,100))
-print(test_Hinv_inverse(v,0.0001,100))
+v=np.ones(200)
+#print(test_Hinv_inverse(v,0.0001,100))
 
-#v=np.ones((50,50))
+v=np.ones((50,50))
 #print(test_Hinv_inverse(v,0.0001,100))
 
 
-''' --- #Flo T ## test if really eigenvalues/vectors --- '''
 
+''' --- #Flo T ## test if really eigenvalues/vectors --- '''
 def test_eigenvalue_vector(result_arnoldi):   
     # habe jetzt entschieden das als input zu nehmen, da wir letzlich ja andere Tests vorher machen können um zu testen ob immer die Eigenwerte rauskommen
     # sonst müssen wir jedes mal die arnoldi Funktion bei den Tests ausführen, so führen wir sie einmal vor allen Tests aus
-    size = (result_arnoldi[0]).shape
+    size = len(result_arnoldi[0])
     err = []
     for i in np.arange(size):
         LHS = hamiltonian.hamilton(result_arnoldi[1][i])
         RHS = result_arnoldi[0][i]*result_arnoldi[1][i]
-        error = np.abs(LHS-RHS)
-        err.append(error)
+        max_error = np.max(np.abs(LHS-RHS))
+        err.append(max_error)
     return err
 
-v = np.ones(10)
+
+v = np.ones(200)
+#result_arnoldi = eigenmethods.arnoldi(v,5,0.0001,100,0.0001,100)
 print(test_eigenvalue_vector(eigenmethods.arnoldi(v,5,0.0001,100,0.0001,100)))
+
+
+
+
+
+
 
 ###### Tests:
 #Flo H ## test if really orthonormal
