122c94e63ce900b9fc50f30573d82f4831ba98ec
FloTell <floriantelleis@web.de>
2024-12-10 18:22:30 +0100

    added arnoldi and changed for modularity

--- Eigenmethods.py
+++ Eigenmethods.py
@@ -1,6 +1,4 @@
 import numpy as np
-from scipy import stats 
-
 
 import variables
 from variables import *
@@ -9,14 +7,12 @@ import hamiltonian
 
 """ --- conjugate gradient --- """
 # A x = b
-# H x = v -> x = H^-1 v
-# so conjugate gradient gives x as result which is our wanted quantity
+# H x = v -> x = H^-1 v     # so conjugate gradient gives x as result which is our wanted quantity
 # now: implement hamiltonian in function itself
-
 
 def Hinv(v,tolerance,maxiters):
     D = len(v.shape)
-    x0 = np.zeros((N,)*D)   # D-dimensional --- can be changed to x0 = np.zeros(N) if only works in 1D!!!
+    x0 = np.zeros((N,)*D)   # D-dimensional --- can be changed to x0 = np.zeros(N) if only works in 1D
     r0 = v - hamiltonian.hamilton(x0)
     if np.max(r0) <= tolerance:    
         return x0
@@ -33,15 +29,77 @@ def Hinv(v,tolerance,maxiters):
         r0 = r
 
 
-####### for using the result in Arnoldi method: use Hinv()[0] because I am also returning the iteration as second argument
 
+''' --- Arnoldi method --- '''
 
+def norm(vector):
+    return np.sqrt(np.vdot(vector,vector))
 
+def matrix_multi(vector, iterations, error_Hinv, maxiters_Hinv):
+    for i in range(iterations):
+        w = Hinv(vector, error_Hinv, maxiters_Hinv)[0]
+        vector = w
+    return vector
+
+def gram_schmidt(array):
+    space = []
+    for i in range(len(array)):
+        w = array[i]
+        a = w
+        for j in range(len(space)):
+            a += -np.vdot(space[j], w)*space[j]
+        a = a/norm(a)
+        space.append(a)
+    return space
+        
+def krylov_space(vector, number_eigen, error_Hinv, maxiters_Hinv):
+    space = []
+    for i in range(number_eigen):
+        space.append(matrix_multi(vector, i, error_Hinv, maxiters_Hinv))
+    return space 
+
+def matrix_once(array, error_Hinv, maxiters_Hinv):
+    space = []
+    for i in range(len(array)):
+        space.append(matrix_multi(array[i], 1, error_Hinv, maxiters_Hinv))
+    return space 
+
+def eigenvalues(array, error_Hinv, maxiters_Hinv):
+    space = []
+    for i in range(len(array)):
+        w = array[i]
+        eigen = np.vdot(w, matrix_multi(w, 1, error_Hinv, maxiters_Hinv))
+        space.append(eigen)
+    return space
+
+def arnoldi(number_eigen, error_arnoldi, maxiter_arnoldi, error_Hinv, maxiters_Hinv):
+    v = np.ones(N)
+    vectors = krylov_space(v, number_eigen, error_Hinv, maxiters_Hinv)
+    for count, i in enumerate(range(maxiter_arnoldi)):
+        errors = []
+        vectors = matrix_once(vectors, error_Hinv, maxiters_Hinv)
+        orth_vectors = gram_schmidt(vectors)
+        eigen = eigenvalues(orth_vectors, error_Hinv, maxiters_Hinv)
+        for i in range(len(vectors)):
+            LHS = matrix_multi(orth_vectors[i], 1, error_Hinv, maxiters_Hinv)
+            RHS = eigen[i]*orth_vectors[i]
+            error = norm(LHS - RHS)
+            errors.append(error)
+        if np.all(np.array(errors)) <  error_arnoldi:
+            return 1/np.array(eigen),orth_vectors
+            #print('Found eigenvalues')
+        else:                       # same as mine: do we have to do extra step for <maxiters or output "None" enough?
+            vectors = orth_vectors
+            #print(np.max(errors))
+            continue
 
 
 
 
 """ --- test the code --- """
+
+#print(arnoldi(5,0.0001,100,0.0001,100))   
+
 
 # tests for arbitrary v
 #### 1D test
