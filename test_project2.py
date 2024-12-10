9a4c89b5e72511d7c6daa4829a61c4f926828b06
FloTell <floriantelleis@web.de>
2024-12-10 18:04:20 +0100

    new test idea

--- test_project2.py
+++ test_project2.py
@@ -1,6 +1,5 @@
 
 
-
 #Flo T ### test if inverse is correct
 
 ###### Tests:
@@ -11,3 +10,9 @@
 
 #Mickey/Flo H ## test with matrices we know eigenvalues/vectors -->> hamiltonians from different systems
 ### .....
+
+
+### some kind of test for checking if chosen error and maxiters is good?    # e.g. test different errors and outcomes
+
+
+
