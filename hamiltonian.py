14ce3abae48af1d33d20cfbcf57dc5a2d702cea8
FloTell <floriantelleis@web.de>
2024-12-20 10:07:23 +0100

    changed error with N in potential/hamiltonian

--- hamiltonian.py
+++ hamiltonian.py
@@ -33,6 +33,19 @@ def hamilton(func):
     return -1/(2*mu*epsilon**2)*laplace(func)+np.multiply(potential(func),func)
 
 
-def hamilton_variable(func,mu,epsilon):     # hamilton defined in the same way as above, but with input mu and epsilon for further usage in eigenmethod tests
+
+''' --- variables mu and epsilon in input for eigenmethods part --- '''
+# potential/hamilton defined in the same way as above, but with input mu and epsilon for further usage in eigenmethod tests
+
+def potential_variable(func,mu,epsilon):
+    """defines the potential"""
+    N = func.shape[0]   # so that also N variable
+    V = np.zeros(func.shape)
+    for n, _ in np.ndenumerate(func):
+        index_arr = np.array(n)
+        V[n]=mu/8*(epsilon**2*np.dot(index_arr-int(N/2),index_arr-int(N/2))-1)**2
+    return V
+
+def hamilton_variable(func,mu,epsilon):     
     """calculating the hamiltonian for double harmonic well"""
-    return -1/(2*mu*epsilon**2)*laplace(func)+np.multiply(potential(func),func)
+    return -1/(2*mu*epsilon**2)*laplace(func)+np.multiply(potential_variable(func,mu,epsilon),func)
