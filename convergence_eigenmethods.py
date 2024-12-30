c5d09d326b7e93a01fec9c66bb7f955a74d5b70c
Flopsiturtle <80710942+Flopsiturtle@users.noreply.github.com>
2024-12-30 23:31:27 +0100

    Update convergence_eigenmethods.py

--- convergence_eigenmethods.py
+++ convergence_eigenmethods.py
@@ -32,7 +32,7 @@ def get_data(number_eigen,start,finish,gap,mu,epsilon):
     x = []
     y = []
     for N in np.arange(start,finish+gap,gap):
-        v = np.ones(N)
+        v = np.random.random(N)
         #print(eigenmethods.arnoldi(v, 1, 0.0001, 100, 0.0001, 200)[0])
         data = eigenmethods.arnoldi(v, number_eigen, 10**(-7), 500, 10**(-7), 500,mu,epsilon)[0]
         if isinstance(data,str) is False: 
@@ -45,7 +45,7 @@ def get_data(number_eigen,start,finish,gap,mu,epsilon):
 mu = 20         # his mu
 epsilon = 1/60
 """
-data = get_data(4,200,400,40,mu,epsilon)     ### maybe delete input with start,finish,gap to be always the same and give input of errors and maxiters, depending on usecase we want for final test
+data = get_data(4,40,240,40,mu,epsilon)     ### maybe delete input with start,finish,gap to be always the same and give input of errors and maxiters, depending on usecase we want for final test
 plt.plot(data[0],data[1],'ro')
 plt.show()
 """
@@ -99,13 +99,32 @@ def plot_parity():
     Eig_val_even, Eig_vec_even = eigenmethods.arnoldi(v_even, 2, 10**(-6), 500, 10**(-6), 500,mu,epsilon)
     Eig_val_odd, Eig_vec_odd = eigenmethods.arnoldi(v_odd, 2, 10**(-6), 500, 10**(-6), 500,mu,epsilon)
     plot_eigenvectors(np.concatenate((Eig_val_even,Eig_val_odd)),np.concatenate((Eig_vec_even,Eig_vec_odd)))
-
+"""
 v = np.random.random(201)
 Eig_val, Eig_vec = eigenmethods.arnoldi(v, 4, 10**(-6), 500, 10**(-6), 500,mu,epsilon)
 plot_eigenvectors(Eig_val, Eig_vec)
 plot_parity()
+"""
 
+def continuum():
+    multi = np.arange(1,11, 2)
+    Eig_val, Eig_vec = [], []
+    for m in multi:
+        N = 201 * m
+        eps = epsilon/m
+        v = np.concatenate((np.random.random(N//2),[0])) 
+        v_even = np.concatenate((v,list(reversed(v[:-1]))))
+        v_odd = np.concatenate((-v,list(reversed(v[:-1]))))
+        print(m, N, eps)
+        print(v_even)
+        Eig_val_even, Eig_vec_even = eigenmethods.arnoldi(v_even, 2, 10**(-6), 500, 10**(-6), 500,mu,eps)
+        Eig_val_odd, Eig_vec_odd = eigenmethods.arnoldi(v_odd, 2, 10**(-6), 500, 10**(-6), 500,mu,eps)
+        Eig_val.append(np.concatenate((Eig_val_even,Eig_val_odd)))
+        Eig_vec.append(np.concatenate((Eig_vec_even,Eig_vec_odd)))
+    return Eig_val, Eig_vec
 
+plt.plot(np.arange(1,11, 2), continuum()[0])
+plt.show()
 #tol_evol(4)
 ### choose a value for N for which we have already converged in the infinite volume limit
 ### lets say N_0 = 200 for epsilon_0 = 1/60
