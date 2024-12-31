625e6cb954e9cf089fd054f92ed9abc0162dae88
FloTell <floriantelleis@web.de>
2024-12-31 15:13:41 +0100

    cleared file and added comments

--- test_eigenmethods.py
+++ test_eigenmethods.py
@@ -4,14 +4,7 @@ import eigenmethods
 import hamiltonian
 
 
-######
-############## for our tests in latex protocol, explain what variables chosen and also do tests with different parameters: shape, error_Hinv
-######
-
-
-''' --- #Flo T ### test if inverse is correct --- '''
-#### test works in multiple dimensions (Hinv and hamiltonian in different dimensions)
-def test_Hinv_inverse(shape_input,iterations,error_Hinv,maxiters_Hinv,mu,epsilon):
+def test_Hinv_inverse(shape_input,iterations,error_Hinv,maxiters_Hinv,mu,epsilon):      #### test works in multiple dimensions (Hinv and hamiltonian in different dimensions)
     shape = shape_input.shape
     err = []
     for i in range(iterations):
@@ -21,11 +14,7 @@ def test_Hinv_inverse(shape_input,iterations,error_Hinv,maxiters_Hinv,mu,epsilon
     return np.max(err)
 
 
-
-''' --- #Flo T ## test if really eigenvalues/vectors --- '''
-def test_eigenvalue_vector(result_arnoldi):   
-    # habe jetzt entschieden das als input zu nehmen, da wir letzlich ja andere Tests vorher machen können um zu testen ob immer die Eigenwerte rauskommen
-    # sonst müssen wir jedes mal die arnoldi Funktion bei den Tests ausführen, so führen wir sie einmal vor allen Tests aus
+def test_eigenvalue_vector(result_arnoldi):     # <-- do arnoldi one time before and use as input
     size = len(result_arnoldi[0])
     err = []
     for i in np.arange(size):
@@ -36,10 +25,7 @@ def test_eigenvalue_vector(result_arnoldi):
     return err
 
 
-
 def test_orthonormality(vectors):
-    #sollte klappen mit den richtigen inputs, ich konnte es jetzt aber noch nicht für arnoldi testen
-    #output ist error matrix, idk wie oder ob das in mehr als einer dimension klappt
     matrix = np.column_stack(vectors)
     gram = np.dot(matrix.T, matrix)
     identity = np.eye(len(vectors))
@@ -47,14 +33,56 @@ def test_orthonormality(vectors):
 
 
 
+#Mickey ## test if really lowest eigenvalue
+
+
+
+
+## test our results with linalgs programs (?)
+
+
+
+
+##### Mickey/Flo H ## test with matrices we know eigenvalues/vectors -->> hamiltonians from different systems
+##### xxx we decided not to
+
+
+
+
+## maybe animate evolution of different eigenstates? - Flo H   -- what do you mean? isnt that in convergence? - Flo T
+
+
+
+
+# some kind of test for checking if chosen error and maxiters is good?    # e.g. test different errors and outcomes   
+
+
+
+
+
+###### also if program is run multiple times, is convergence the same? what error if not --> well depends on chosen error of course...
 
 
 
 
 ''' --- test the code --- '''
-mu = 153.9     # our mu
-#mu = 20         # his mu
+############ he thought all these test ideas are good
+######
+############## for our tests in latex protocol, explain what variables chosen and also do tests with different parameters: shape, error_Hinv
+######
+
+
+#mu = 153.9     # our mu
+mu = 20         # his mu
 epsilon = 1/60
+
+
+
+
+
+
+
+exit()
 
 
 #test inverse 1D
@@ -82,21 +110,6 @@ print(np.max(test_orthonormality(result_arnoldi[1])))
 
 
 
-###### Tests:
-#Flo H ## test if really orthonormal
-#Mickey ## test if really lowest eigenvalue
-#xxx ## test our results with linalgs programs (?)
-
-#Mickey/Flo H ## test with matrices we know eigenvalues/vectors -->> hamiltonians from different systems
-### .....
-
-############ he thought all these test ideas are good
-
-### maybe animate evolution of different eigenstates? - Flo H
-
-### some kind of test for checking if chosen error and maxiters is good?    # e.g. test different errors and outcomes   
-
-### also if program is run multiple times, is convergence the same? what error if not --> well depends on chosen error of course...
 
 
 
