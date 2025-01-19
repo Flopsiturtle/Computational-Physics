9bbeacc34bf434115f2c95821407a69809f088c7
FloTell <floriantelleis@web.de>
2025-01-19 08:48:51 +0100

    Fixed my function

--- prob_distr.py
+++ prob_distr.py
@@ -22,7 +22,7 @@ probs_array = np.loadtxt('prob_distr results/test_probs2.txt')
 
 
 ############## how do we want to incorporate errors into distribution?
-######### maybe own distribution of errors (easy implementation) - but then no meaning of which values have which errors
+######### maybe distribution of errors (easy implementation) - but then no meaning of which values have which errors
 
 
 
@@ -40,8 +40,9 @@ plt.show()
 
 
 ''' --- my own version --- '''
-# ----- does the same except you have to choose an error for max(array) which can lead to wrong results -> if statement in function...
-
+# ----- does the same
+##### pro for mine: can bd changed to our meaning for error implementation
+##### con for minr: maybe slower, does not easily change between histo and density (fix: number/probs.shape)
 def prob_distr(probs_array,num_bars):
     probs, probs_err = probs_array
     max = np.max(probs)
@@ -49,10 +50,13 @@ def prob_distr(probs_array,num_bars):
     bar_size = (max-min)/num_bars
     bars = []
     for i in np.arange(num_bars):
-        number = probs[(probs>=(min+bar_size*i)) & (probs<(max+10**(-10)+bar_size*(i+1-num_bars)))]
+        number = probs[(probs>=(min+bar_size*i)) & (probs<(max+bar_size*(i+1-num_bars)))]
         bars.append(number.size)
+    bars[num_bars-1] += 1
     bars.append(0)
     x = np.arange(min,max+bar_size,bar_size)
+    return x,bars,bar_size
+    #### below was for different code with error
     if np.sum(bars) == probs.shape:
         return x,bars,bar_size
     else:
@@ -73,7 +77,6 @@ plt.show()                                   # ansonsten die Standard library zu
 
 
 ''' --- distributiuon using gaussian kernel --- '''
-
 ####### cool but problem: wants to be zero at the edges! - could work for our values because gaussian i think      (even more at right because of the zero at end)
 data = probs_array[0]
 kde = gaussian_kde(data)
