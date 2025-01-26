fa7e24e48ac4f09b967669f25f8bce922ecb9480
FloTell <floriantelleis@web.de>
2025-01-26 13:09:13 +0100

    finalized functions - plots need labels

--- prob_distr.py
+++ prob_distr.py
@@ -8,36 +8,7 @@ from numpy import linspace
 
 
 
-### create and save test arrays
-np.random.seed(1)
-test_probs = np.random.normal(10,1,(2,500)) # mean 10 with st_dev 1
-test_probs[1]=(np.random.rand(1,500))   # errors not gaussian
-#np.random.seed(20)
-#test_probs2 = np.random.rand(2,500)
-#np.savetxt('test_probs_gauss.txt',test_probs)
-
-
-#plt.bar(bincenters, y, width=bar_size, color='r', yerr=menStd)
-#plt.show()
-
-
-############## how do we want to incorporate errors into distribution?
-######### maybe distribution of errors (easy implementation) - but then no meaning of which values have which errors
-#----- he says try bootstrap method or doing way more replicas and then statistical mean (same as before from lecture) over means -> use overall mean as value for histogramm and error of value as error for histrogramm
-
-
-
-
-
-
-''' --- easiest way for a histogram --- '''
-#weights = np.ones_like(data)/float(len(data))   # add weights=weights into plt.hist() for density plot
-#plt.hist(data,density=False, bins=num_bars, color='green')         # , histtype='step'
-#plt.xticks(.....) #would have to be with max/min -> my function
-#plt.show()
-
-
-''' --- bootstrap --- '''
+''' --- implementing bootstrap-method for our 500 replicas --- '''
 
 def bootstrap_samples(data,numb_samples,size_small_sample):
     boot_samples = []
@@ -68,52 +39,66 @@ def mean_error_hist(boot_samples,num_bars):
 
 
 
-''' run the code '''
-### initiate which array
-probs_array = np.loadtxt('prob_distr results/test_probs_gauss2.txt')
-data = probs_array[0]
-num_bars = 50
+''' --- function for plotting the final histogram  --- '''
 
-###
+def final_histogramm(data,calc_mean_mean,num_bars,numb_samples,size_small_sample):
+    ### data
+    boot_samples = bootstrap_samples(data,numb_samples,size_small_sample)
+    data_boots,error_boots,binEdges = mean_error_hist(boot_samples,num_bars)
+    bar_centers = 0.5*(binEdges[1:]+binEdges[:-1])
+    bar_size = ((np.max(binEdges)-np.min(binEdges))/num_bars)
+    ### plotting
+    plt.figure(figsize=(11,7))
+    plt.bar(bar_centers, data_boots, width=bar_size, color=['cornflowerblue','royalblue'], label="bars")    # histogram from bootstrap
+    plt.errorbar(bar_centers, data_boots, fmt=" ", yerr=error_boots, color='black', capsize=2, label="error")      # error-bars from bootstrap
+    plt.axvline(x=calc_mean_mean, color='red', label="mean-mean calc")       # visaulizing the bootstrap calculated mean
+    plt.axvline(x=np.mean(boot_samples), linestyle='dashed', color='limegreen', label="mean-mean boot")       # visaulizing the beforehand calculated mean
+    plt.legend(loc="upper left")
+    plt.xlabel('x')
+    plt.ylabel('y')
+    plt.title('#bars = {0}, #boot samples = {1}, size small samples = {2}'.format(num_bars,numb_samples,size_small_sample))
+    plt.show()
+
+
+''' --- run the code --- '''
+### initiate which array
+probs_array_magn = np.loadtxt('prob_distr results/test_probs_gauss2.txt') # change for magn
+data_magn = probs_array_magn[0]
+probs_array_energy = np.loadtxt('prob_distr results/test_probs_gauss2.txt') # change for energy
+data_energy = probs_array_energy[0]
+### define what is the beforehand calculated mean
+calc_mean_mean_magn = 10
+calc_mean_mean_energy = 10
+
+### set number of bars for histograms
+num_bars = 50
+### set parameters for bootstrap-method
 numb_samples = 50
 size_small_sample = 5
 
-boot_samples = bootstrap_samples(data,numb_samples,size_small_sample)
-data_boots,error_boots,binEdges = mean_error_hist(boot_samples,num_bars)
-bar_centers = 0.5*(binEdges[1:]+binEdges[:-1])
-bar_size = ((np.max(binEdges)-np.min(binEdges))/num_bars)
-plt.bar(bar_centers, data_boots, width=bar_size, color=['cornflowerblue','royalblue'], yerr=error_boots)
-plt.title('#bars = {0}, #boot samples = {1}, size small samples = {2}'.format(num_bars,numb_samples,size_small_sample))
-plt.show()
-
-###### also good idea: draw one vertical line with our calculated mean mean !!!!!!!!
+# magnetization
+final_histogramm(data_magn,calc_mean_mean_magn,num_bars,numb_samples,size_small_sample)
+# energy
+final_histogramm(data_energy,calc_mean_mean_energy,num_bars,numb_samples,size_small_sample)
 
 
-
-#plt.bar(bar_centers, data_boots, width=bar_size, yerr=error_boots,facecolor='k',alpha=0.1)
-#plt.step(bar_centers, data_boots,'k',linestyle='--',linewidth=1)
 
 
 
 exit()
 
-
-
-
-
-''' --- test with the other forms --- '''
-'''distributiuon using gaussian kernel'''
-####### cool but problem: wants to be zero at the edges! - could work for our values because gaussian i think      (even more at right because of the zero at end)
-data = probs_array[0]
+''' --- distributiuon using gaussian kernel --- '''
 y,binEdges = np.histogram(data,bins=num_bars)
-
 kde = gaussian_kde(data)
 dist_space = linspace(min(data),max(data),100)
-plt.plot(dist_space,kde(dist_space)/np.max(kde(dist_space))*(np.max(y)), color='red')
+plt.plot(dist_space,kde(dist_space)/np.max(kde(dist_space))*(np.max(y)), color='purple')
+plt.show()
 
 
 
-'''distributiuon using original data'''
+exit()
+
+''' --- for checking: histogram using original replicas without bootsrap --- '''
 bar_centers = 0.5*(binEdges[1:]+binEdges[:-1])
 bar_size = ((np.max(binEdges)-np.min(binEdges))/num_bars)
 plt.bar(bar_centers,y, width=bar_size, color='green')         # , histtype='step'
@@ -122,14 +107,53 @@ plt.show()
 
 
 
+
+
+
+
+
+
+
+
+
+
+
 exit()
 
 
 
 
 
+### create and save test arrays
+#np.random.seed(1)
+#test_probs = np.random.normal(10,1,(2,500)) # mean 10 with st_dev 1
+#test_probs[1]=(np.random.rand(1,500))   # errors not gaussian
+#np.random.seed(20)
+#test_probs2 = np.random.rand(2,500)
+#np.savetxt('test_probs_gauss.txt',test_probs)
 
 
+#plt.bar(bincenters, y, width=bar_size, color='r', yerr=menStd)
+#plt.show()
+
+
+############## how do we want to incorporate errors into distribution?
+######### maybe distribution of errors (easy implementation) - but then no meaning of which values have which errors
+#----- he says try bootstrap method or doing way more replicas and then statistical mean (same as before from lecture) over means -> use overall mean as value for histogramm and error of value as error for histrogramm
+
+
+
+''' --- easiest way for a histogram --- '''
+#weights = np.ones_like(data)/float(len(data))   # add weights=weights into plt.hist() for density plot
+#plt.hist(data,density=False, bins=num_bars, color='green')         # , histtype='step'
+#plt.xticks(.....) #would have to be with max/min -> my function
+#plt.show()
+
+
+
+
+#plt.bar(bar_centers, data_boots, width=bar_size, yerr=error_boots,facecolor='k',alpha=0.1)
+#plt.step(bar_centers, data_boots,'k',linestyle='--',linewidth=1)
 
 
 
@@ -166,9 +190,3 @@ def prob_distr(probs_array,num_bars,density):
 
 
 #exit()
-
-
-
-
-
-
