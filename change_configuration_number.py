29e8c9e82191c88bf8b5d2bc39b594c32bf6db70
Mickeywilke <116799230+Mickeywilke@users.noreply.github.com>
2025-02-13 01:54:07 +0100

    Update change_configuration_number.py

--- change_configuration_number.py
+++ change_configuration_number.py
@@ -54,36 +54,39 @@ y = f(x, *popt)
 ax1.plot(x, stat_error, label = 'Energy error')
 ax3.plot(x, linearised, label = 'linearised energy error \n of the form $1/y^2 $')
 ax3.plot(x, y, label =  'linear fit $f=mx+n$')
-#print(popt, pcov)
+print('Energy:')
+print('m = ' + str(popt[0]) + ' +/- ' + str(np.sqrt(pcov[0][0])))
+print('n = ' + str(popt[1]) + ' +/- ' + str(np.sqrt(pcov[1][1])))
+
 
 
 'magnetisation'
 
 
 column_names = ['replica_number' + str(i+1) for i in range(R+1)]
-energydata =  pd.read_csv('C:\\Users\\Mickey Wilke\\Desktop\\cp2_ising\\EnergyReplica.csv', names = column_names, index_col=False) #change path accordingly
-energydata = energydata.drop(['replica_number' + str(R+1)], axis = 1)
-energydata.index = energydata.index + 1
-x = energydata.index.values
-indexnames = energydata[energydata.index < Nth].index
-energydata.drop(indexnames, inplace =True)
+magdata =  pd.read_csv('C:\\Users\\Mickey Wilke\\Desktop\\cp2_ising\\MagnetizationReplica.csv', names = column_names, index_col=False) #change path accordingly
+magdata = magdata.drop(['replica_number' + str(R+1)], axis = 1)
+magdata.index = magdata.index + 1
+x = magdata.index.values
+indexnames = magdata[magdata.index < Nth].index
+magdata.drop(indexnames, inplace =True)
 stat_error = []
 linearised = []
 for i in range(u):
-    w = energydata.copy()
-    indexnames = energydata[energydata.index > Nth + i].index
+    w = magdata.copy()
+    indexnames = magdata[magdata.index > Nth + i].index
     w.drop(indexnames, inplace =True)
     
-    mean_energies = []
+    mean_mag = []
     for i in range(R):
         values = w['replica_number' + str(i+1)]
         mean = np.sum(values)/len(values)
-        mean_energies.append(mean)
+        mean_mag.append(mean)
         
 
     
-    exp_value = 1/len(mean_energies) * np.sum(mean_energies)
-    deviation = (mean_energies - exp_value)**2
+    exp_value = 1/len(mean_mag) * np.sum(mean_mag)
+    deviation = (mean_mag - exp_value)**2
     stat_err = np.sqrt(np.sum(deviation) / (R*(R - 1)))
     stat_error.append(stat_err)
     linearised.append(1/(stat_err)**2)
@@ -94,7 +97,10 @@ y = f(x, *popt)
 ax2.plot(x, stat_error, label = 'Magnetisation error', color = 'r')
 ax4.plot(x, linearised , label = 'linearised magnetisation error \n of the form $1/y^2 $',  color = 'r')
 ax4.plot(x, y, label =  'linear fit $f=mx+n$', color = 'g')
-#print(popt, pcov)
+print('Magnetisation:')
+print('m = ' + str(popt[0]) + ' +/- ' + str(np.sqrt(pcov[0][0])))
+print('n = ' + str(popt[1]) + ' +/- ' + str(np.sqrt(pcov[1][1])))
+
 
 
 
@@ -102,6 +108,5 @@ ax1.legend(fontsize = 10)
 ax2.legend(fontsize = 10)
 ax3.legend(fontsize = 10, loc = 'upper left')
 ax4.legend(fontsize = 10, loc = 'upper left')
-
 
 
